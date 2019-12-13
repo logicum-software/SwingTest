7debc2118da40dbb09d7c088038967324c13f71d
Markus Gärtner <gaertner.brilon@gmail.com>
2019-12-13 05:50:56 +0100

    20191213 05:50 MG

--- src/CalculatingPrimesThread.java
+++ src/CalculatingPrimesThread.java
@@ -2,14 +2,14 @@ public class CalculatingPrimesThread extends Thread {
     public void run() {
 
         int nPrimes = 0;
-        float fMod = 0;
 
-        for (int i = 3; i < 100000; i++) {
+        for (int i = 3; i < 10000; i++) {
             for (int z = 2; z < i; z++) {
                 if (i % z == 0) {
-                    nPrimes++;
                     break;
                 }
+                if (z == i - 1)
+                    nPrimes++;
             }
         }
         System.out.println(nPrimes);
